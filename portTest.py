46cea14451230535513e61f71a1aa8023e86f809
rodrigofasllima <149618734+rodrigofasllima@users.noreply.github.com>
2025-11-20 20:03:32 -0300

    Add portTest.py
    This script checks if a host is online and scans specified ports for TCP connections.

---
+++ portTest.py
@@ -0,0 +1,44 @@
+import socket
+from ping3 import ping
+
+############################################
+#      Verifica se host esta online        #
+############################################
+def online(alvo):
+    if(ping(alvo)):
+        return True
+    return False
+
+#############################################
+#          testa porta do host              #
+#############################################
+def scan_port(alvo, porta):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(2)
+
+    try:
+        resposta = sock.connect_ex((alvo, porta))
+        sock.close()
+        return resposta == 0
+    except TimeoutError:
+        return False
+    except Exception as e:
+        print(f"Erro: {e}")
+
+# Lista de portas a verifica
+portas = list({22, 23, 53, 80, 1315, 179})
+portas.sort()
+
+for octeto1 in range(0, 2, 1):
+    for octeto2 in range(0, 255, 1):
+        alvo = f"10.0.{octeto1}.{octeto2}"
+        print(f"\033[34mProcurando em {alvo}\033[0m")
+        if(online(alvo)):
+            for porta in portas:
+                print(f"\tconexão TCP posta {porta}\t", end='')
+                if (scan_port(alvo, porta)):
+                    print(f"\033[32m[ OPEN ]\033[0m")
+                else:
+                    print(f"\033[31m[CLOSED]\033[0m")
+        else:
+            print(f"\t\t\t\t\033[33m[OFFLIN]\033[0m")
